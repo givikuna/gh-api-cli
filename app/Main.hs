7af42e54b6f230c8e511721e97e8e46e00826966
Givi <gvtsvariani@gmail.com>
2026-08-13 20:54:51 +0000

    basic parser set up

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,25 @@
 
 module Main (main) where
 
-import System.Environment (getArgs)
+import           CLI.Parser         (AppConfig (..), defaultConfig, parseArgs)
+import           System.Environment (getArgs)
+import           System.Exit        (exitFailure)
 
-import CLI.Parser (Command (..), parseArgs)
 
 main :: IO ()
 main = do
     args :: [String] <- getArgs
-    let command :: Command = parseArgs args
-    print command
+    let config :: AppConfig = parseArgs args defaultConfig
+    if cfgShowHelp config then do
+        putStrLn "Usage: gh-api-cli --user<username> [TARGET] [OPTIONs]"
+        putStrLn "Targets: --total-stars, --repos, --followers, --most-used-lang"
+    else if cfgShowVer config then do
+        putStrLn "gh-api-cli v0.1.0.0"
+    else
+        case cfgUser config of
+        Nothing -> do
+            putStrLn "ERROR: Provide an username. Example: --user=KrakenTheJacken"
+            exitFailure
+        Just username -> do
+            putStrLn $ "Target acquired: " ++ username
+            putStrLn $ "Action requested: " ++ show (cfgTarget config)
--- src/CLI/Parser.hs
+++ src/CLI/Parser.hs
@@ -2,21 +2,54 @@
 
 module CLI.Parser where
 
-import Data.List (stripPrefix)
+import           Data.List (stripPrefix)
 
-data Command
-    = Help
-    | Version
-    | GetUser String
-    | Unknown String
+data DataTarget
+    = TotalStars
+    | MostUsedLang
+    | Repos
+    | Followers
+    | UnspecifiedTarget
     deriving (Show)
 
-parseArgs :: [String] -> Command
-parseArgs [] = Help
-parseArgs ("--help" : _) = Help
-parseArgs ("--version" : _) = Version
-parseArgs ((arg :: String) : (_ :: [String])) =
-    let maybeUsername :: Maybe String = stripPrefix "--user=" arg
-     in case maybeUsername of
-            Just (username :: String) -> GetUser username
-            Nothing -> Unknown arg
+data AppConfig = AppConfig
+    { cfgUser     :: Maybe String
+    , cfgTarget   :: DataTarget
+    , cfgToken    :: Maybe String
+    , cfgFormat   :: String
+    , cfgLimit    :: Int
+    , cfgShowHelp :: Bool
+    , cfgShowVer  :: Bool
+    }
+    deriving (Show)
+
+defaultConfig :: AppConfig
+defaultConfig = AppConfig
+    { cfgUser     = Nothing
+    , cfgTarget   = UnspecifiedTarget
+    , cfgToken    = Nothing
+    , cfgFormat   = "table"
+    , cfgLimit    = 1000
+    , cfgShowHelp = False
+    , cfgShowVer  = False
+    }
+
+parseArgs :: [String] -> AppConfig -> AppConfig
+parseArgs [] config = config
+parseArgs (arg : rest) config =
+    case arg of
+        "--help"        -> parseArgs rest (config { cfgShowHelp = True })
+        "--version"     -> parseArgs rest (config { cfgShowVer = True })
+        "--total-stars" -> parseArgs rest (config { cfgTarget = TotalStars })
+        "--repos"       -> parseArgs rest (config { cfgTarget = Repos })
+        _
+            | Just username <- stripPrefix "--user=" arg  ->
+                parseArgs rest (config { cfgUser = Just username })
+            | Just token    <- stripPrefix "--token=" arg ->
+                parseArgs rest (config { cfgToken = Just token })
+            | Just format   <- stripPrefix "--format=" arg ->
+                parseArgs rest (config { cfgFormat = format })
+            | Just limitStr <- stripPrefix "--limit=" arg ->
+                parseArgs rest (config { cfgLimit = read limitStr })
+            | otherwise                                   ->
+                parseArgs rest config
